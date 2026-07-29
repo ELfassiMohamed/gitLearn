ea97ca53ad214060db003e73c556dc1d06a2fe16
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-07-29 21:23:23 +0100

    strings operations

---
+++ StringOps.java
@@ -0,0 +1,26 @@
+public class StringOps {
+	public final static void main (String[] args){
+//Using String Class
+	
+	String str = "Hello";
+	str = "Hellooooooooooo";
+	
+	String mot = str ;
+
+	//str = "Hellooooooooooo";
+	
+	System.out.println(str); //Hellooooooooooo
+	System.out.println(mot); //Hello
+	
+	
+//Using StringBuilder Class
+	/*
+	StringBuilder a = new StringBuilder("abc");
+	//StringBuilder b = a.append("de");
+	StringBuilder b = new StringBuilder(a.append("de"));
+	b = b.append("f").append("g");
+	System.out.println("a=" + a);
+	System.out.println("b=" + b);
+	*/
+	 
+} }
