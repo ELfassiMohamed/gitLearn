9be090c9d0c764c6fd227465936ff7d0320eaca1
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-09-04 20:49:15 +0100

    fixing a mistake

---
+++ StringOps.java
@@ -0,0 +1,21 @@
+public class StringOps {
+	public final static void main (String[] args){
+//Using String Class
+	
+	String str = "Hello";
+	str = "Hellooooooooooo";
+	
+
+	
+	
+//Using StringBuilder Class
+	/*
+	StringBuilder a = new StringBuilder("abc");
+	//StringBuilder b = a.append("de");
+	StringBuilder b = new StringBuilder(a.append("de"));
+	b = b.append("f").append("g");
+	System.out.println("a=" + a);
+	System.out.println("b=" + b);
+	*/
+	 
+} }
